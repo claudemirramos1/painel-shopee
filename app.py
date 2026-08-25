3ed5ebcf5aa2d0561cc32a3210866943d3c69e64
claudemirramos1 <emanueledime@gmail.com>
2026-08-25 08:42:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import requests
 import json
 import os
 from gtts import gTTS
-from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips
+
+# Importações seguras do MoviePy para evitar erros no Streamlit Cloud
+from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+from moviepy.video.compositing.concatenate import concatenate_videoclips
+from moviepy.video.VideoClip import ImageClip
 
 # ==========================================
 # CONFIGURAÇÕES FIXAS
